a368b047182e4154f8f911096f28020e2ffc2f88
Samar AYADI <ayadisamardev@gmail.com>
2025-05-01 17:48:26 +0100

    feat: create admin dashboard page layout

--- frontend/src/pages/Admin/Dashboard.jsx
+++ frontend/src/pages/Admin/Dashboard.jsx
@@ -1,14 +1,21 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { useUserAuth } from "./../../hooks/useUserAuth";
 import { UserContext } from "../../context/userContext";
+import DashboardLayout from "../../components/layouts/DashboardLayout";
+import { useNavigate } from 'react-router-dom';
 
 const Dashboard = () => {
   useUserAuth();
 
   const { user } = useContext(UserContext);
-  return <div>Dashboard</div>;
 
- 
+  const navigate = useNavigate();
+
+  const [dashboardData, setDashboardData] = useState(null);
+  const [pieChartData, setPieChartData] = useState([]);
+  const [barChartData, setBarChartData] = useState([]);
+
+  return <DashboardLayout activeMenu="Dashboard"></DashboardLayout>;
 };
 
 export default Dashboard;
