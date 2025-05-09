c8f73770005adbece63f6f4c4d04d731077b11e8
Samar AYADI <ayadisamardev@gmail.com>
2025-05-09 22:44:46 +0100

    feat: build and validate Create Task form

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,6 +15,7 @@ import ViewTaskDetails from "./pages/User/ViewTaskDetails";
 
 import PrivateRoute from "./routes/PrivateRoute";
 import UserProvider, { UserContext } from "./context/userContext";
+import { Toaster } from "react-hot-toast";
 
 const App = () => {
   return (
@@ -47,6 +48,15 @@ const App = () => {
           </Routes>
         </Router>
       </div>
+
+       <Toaster
+        toastOptions={{
+          className: "",
+          style: {
+            fontSize: "13px",
+          },
+        }}
+      />
     </UserProvider>
   );
 };
