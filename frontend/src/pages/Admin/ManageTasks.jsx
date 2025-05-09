d164a42960f516416de2c708ef6ff40d077303de
Samar AYADI <ayadisamardev@gmail.com>
2025-05-09 23:53:47 +0100

    feat: display task cards in responsive grid using TailwindCSS

--- frontend/src/pages/Admin/ManageTasks.jsx
+++ frontend/src/pages/Admin/ManageTasks.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React from "react";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
@@ -6,7 +7,8 @@ import { useEffect } from "react";
 import { API_PATHS } from "../../utils/apiPaths";
 import axiosInstance from "./../../utils/axiosInstance";
 import { LuFileSpreadsheet } from "react-icons/lu";
-import TaskStatusTabs from './../../components/TaskStatusTabs';
+import TaskStatusTabs from "./../../components/TaskStatusTabs";
+import TaskCard from './../../components/Cards/TaskCard';
 
 const ManageTasks = () => {
   const [allTasks, setAllTasks] = useState([]);
@@ -86,6 +88,29 @@ const ManageTasks = () => {
               </button>
             </div>
           )}
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mt-4">
+          
+          
+          {allTasks?.map((item, index) => (
+            <TaskCard
+              key={item._id}
+              title={item.title}
+              description={item.description}
+              priority={item.priority}
+              status={item.status}
+              progress={item.progress}
+              createdAt={item.createdAt}
+              dueDate={item.dueDate}
+              assignedTo={item.assignedTo?.map((item) => item.profileImageUrl)}
+              attachmentCount={item.attachments?.length || 0}
+              completedTodoCount={item.completedTodoCount || 0}
+              todoChecklist={item.todoChecklist || []}
+              onClick={() => {
+                handleClick(item);
+              }}
+            />
+          ))}
         </div>
       </div>
     </DashboardLayout>
