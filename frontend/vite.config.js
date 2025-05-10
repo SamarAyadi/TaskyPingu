3a24b75f384e41c899c51888e79e43f0a12d0388
Samar AYADI <ayadisamardev@gmail.com>
2025-05-10 07:13:23 +0100

    chore(vite): optimize build by increasing chunk limit and adding manual chunking for React

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),  tailwindcss(),],
-})
+  plugins: [react(), tailwindcss()],
+  build: {
+    chunkSizeWarningLimit: 1000, // increase chunk size limit to 1MB
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom'],
+        }
+      }
+    }
+  }
+});
